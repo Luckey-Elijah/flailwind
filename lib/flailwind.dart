0659798c4842bef74ca4de99ae06230fe12c628a
Elijah Luckey <luckeyelijah112@gmail.com>
2024-09-13 10:28:47 -0400

    moving into own repo

---
+++ lib/flailwind.dart
@@ -0,0 +1,6 @@
+/// Tailwind-like styling in Flutter with text styles and colors.
+library;
+
+export 'src/color_widget_state_property.dart';
+export 'src/text_style_with_context.dart';
+export 'src/theme_context_extension.dart';
